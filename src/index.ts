73b0d5364163712507b376c3b78ffe5a1da4b606
Tramaine Gillus <1573935+tgillus@users.noreply.github.com>
2024-06-29 19:48:38 -0400

    style: format files

--- src/index.ts
+++ src/index.ts
@@ -15,19 +15,31 @@ import { Transpiler } from './filter/transpiler/transpiler.js';
 
 const transpiler = new Transpiler(new Parser(Semantics.build()));
 
+// biome-ignore lint/suspicious/noConsoleLog: <explanation>
 console.log(transpiler.transpile('current = true'));
+// biome-ignore lint/suspicious/noConsoleLog: <explanation>
 console.log(transpiler.transpile('current != false'));
+// biome-ignore lint/suspicious/noConsoleLog: <explanation>
 console.log(transpiler.transpile('current = false'));
+// biome-ignore lint/suspicious/noConsoleLog: <explanation>
 console.log(transpiler.transpile('current != true'));
+// biome-ignore lint/suspicious/noConsoleLog: <explanation>
 console.log(transpiler.transpile('startDate > 2000-08-09'));
+// biome-ignore lint/suspicious/noConsoleLog: <explanation>
 console.log(transpiler.transpile('startDate >= 2000-08-09'));
+// biome-ignore lint/suspicious/noConsoleLog: <explanation>
 console.log(transpiler.transpile('startDate < 2000-08-09'));
+// biome-ignore lint/suspicious/noConsoleLog: <explanation>
 console.log(transpiler.transpile('startDate <= 2000-08-09'));
+// biome-ignore lint/suspicious/noConsoleLog: <explanation>
 console.log(
   transpiler.transpile('startdate >= 2000-08-09 and startdate <= 2015-08-08')
 );
+// biome-ignore lint/suspicious/noConsoleLog: <explanation>
 console.log(transpiler.transpile('current = true and startDate > 2000-08-09'));
+// biome-ignore lint/suspicious/noConsoleLog: <explanation>
 console.log(transpiler.transpile('current != true and startDate < 2000-08-09'));
+// biome-ignore lint/suspicious/noConsoleLog: <explanation>
 console.log(
   transpiler.transpile(
     'current = false and startdate >= 2000-08-09 and startdate <= 2015-08-08'
@@ -45,9 +57,11 @@ const filter = new Filter(
     new Compare(new StartDate(), '<=', new DateValue('2015-08-08'))
   )
 );
+// biome-ignore lint/suspicious/noConsoleLog: <explanation>
 console.log(filter.produce());
 
 const result = new Filter(
   new Compare(new StartDate(), '>', new DateValue('2000-08-3249'))
 ).produce();
+// biome-ignore lint/suspicious/noConsoleLog: <explanation>
 console.log(JSON.stringify(result, undefined, 2));
